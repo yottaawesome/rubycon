130772b5cbb9d6640da3740a3b37375f136886d2
Vasilios Magriplis <etameson@gmail.com>
2019-05-17 20:25:37 +0930

    More class testing

--- src/test-class.rb
+++ src/test-class.rb
@@ -1,13 +1,30 @@
-class Test 
+class Base
+  def initialize
+  end
+
+  def some_method(str)
+    puts str
+  end
+end
+
+# class inheritance
+class Test < Base
   def initialize(name)
-   @msg = "Hello, #{name}!"
+    # instance variable, these aren't inherited
+    @msg = "Hello, #{name}!"
   end
 
   def display
     puts @msg
+  end
+
+  def some_method(str)
+    # class the base method with the same name as the method invoking super()
+    super(str)
   end
 end
 
 # initialize object
 obj = Test.new("Vasilios")
 obj.display
+obj.some_method "Hello"
